d2c8a34c57c94b5b53cb2de3d34b83f82ebb3164
Kostenko Andrey <andrey@MacBook-Pro-Kostenko.local>
2024-12-27 15:18:48 -0300

    add toast (react)  commit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import s from "./SearchBar.module.css";
+import toast, { Toaster } from 'react-hot-toast';
 
 const SearchBar = ({ onSearchChange }) => {
 const [ value, setValue ] = useState("");
@@ -7,9 +8,16 @@ const [ value, setValue ] = useState("");
 const handleSubmit = (evt) => {
   evt.preventDefault();
   onSearchChange(value);
+  if(!value){
+    toast.success("Please fill it out!",{
+      duration: 1000,
+      position: 'top-center',
+      icon: '🤖',
+    })};
   };
   return (
     <header >
+    <Toaster/>
     <form onSubmit={handleSubmit} className={s.searchHeader}>
     <input className={s.searchInput} value={value} onChange={(evt) => setValue(evt.target.value)} name="topic" type="text" autoComplete="off" autoFocus placeholder="Search images and photos" />
     <button className={s.searchBtn} type="submit">Search</button>
@@ -18,4 +26,4 @@ const handleSubmit = (evt) => {
   )
 }
 
-export default SearchBar
+export default SearchBar;
